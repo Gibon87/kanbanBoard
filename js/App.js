c51fc28bed9cad2f2a6f71caee8809f99140106d
Mateusz <mateusz.gibki@gmail.com>
2019-02-26 22:13:10 +0100

    lil change

--- js/App.js
+++ js/App.js
@@ -6,29 +6,29 @@
 // 	"X-Auth-Token": "1fcc5e5e9a372f3b3baeebfb50bd053a"
 // };
 
-function setupColumns(columns) {
-    columns.forEach(function(column) {
-        var col = new Column(column.id, column.name);
-        setupCards(col, column.cards)
-        board.addColumn(col);
-    });
-}
+// function setupColumns(columns) {
+//     columns.forEach(function(column) {
+//         var col = new Column(column.id, column.name);
+//         setupCards(col, column.cards)
+//         board.addColumn(col);
+//     });
+// }
 
-function setupCards(col, cards) {
-    cards.forEach(function(card) {
-        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-        col.addCard(card);
-    });
-}
+// function setupCards(col, cards) {
+//     cards.forEach(function(card) {
+//         var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+//         col.addCard(card);
+//     });
+// }
 
-$.ajaxSetup({
-    headers: myHeaders
-});
+// $.ajaxSetup({
+//     headers: myHeaders
+// });
 
-$.ajax({
-    url: baseUrl + "/board",
-    method: "GET",
-    success: function(response) {
-        setupColumns(response.columns);
-    }
+// $.ajax({
+//     url: baseUrl + "/board",
+//     method: "GET",
+//     success: function(response) {
+//         setupColumns(response.columns);
+//     }
 });
