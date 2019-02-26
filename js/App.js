deee2c7f4842282df336f073e94572db48a6986a
Mateusz <mateusz.gibki@gmail.com>
2019-02-26 22:09:35 +0100

    api change

--- js/App.js
+++ js/App.js
@@ -1,32 +1,34 @@
-var baseUrl = "https://kodilla.com/pl/bootcamp-api";
-var myHeaders = {
-	"X-Client-Id": "1533",
-	"X-Auth-Token": "1fcc5e5e9a372f3b3baeebfb50bd053a"
-};
+// Niestety serwer nie działa dlatego po odświezeniu nie beda widoczne zapisane elementy, sorry :(
+
+// var baseUrl = "https://kodilla.com/pl/bootcamp-api";
+// var myHeaders = {
+// 	"X-Client-Id": "1533",
+// 	"X-Auth-Token": "1fcc5e5e9a372f3b3baeebfb50bd053a"
+// };
 
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
         setupCards(col, column.cards)
         board.addColumn(col);
     });
 }
 
 function setupCards(col, cards) {
-	cards.forEach(function (card) {
-      var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.addCard(card);
-  	});
+    cards.forEach(function(card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.addCard(card);
+    });
 }
 
 $.ajaxSetup({
-	headers: myHeaders
+    headers: myHeaders
 });
 
 $.ajax({
     url: baseUrl + "/board",
     method: "GET",
     success: function(response) {
-      setupColumns(response.columns);
+        setupColumns(response.columns);
     }
 });
