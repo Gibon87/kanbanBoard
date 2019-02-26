748e7eec83e88b2cd06bc389f2e95e57d674992c
Mateusz <mateusz.gibki@gmail.com>
2019-02-26 22:22:27 +0100

    26.02

--- js/App.js
+++ js/App.js
@@ -1,34 +1,34 @@
-// Niestety serwer nie działa dlatego po odświezeniu nie beda widoczne zapisane elementy, sorry :(
+// Niestety serwer nie działa dlatego narazie apliakcja sie wysypała, sorry :(
 
-// var baseUrl = "https://kodilla.com/pl/bootcamp-api";
-// var myHeaders = {
-// 	"X-Client-Id": "1533",
-// 	"X-Auth-Token": "1fcc5e5e9a372f3b3baeebfb50bd053a"
-// };
+var baseUrl = "https://kodilla.com/pl/bootcamp-api";
+var myHeaders = {
+    "X-Client-Id": "1533",
+    "X-Auth-Token": "1fcc5e5e9a372f3b3baeebfb50bd053a"
+};
 
-// function setupColumns(columns) {
-//     columns.forEach(function(column) {
-//         var col = new Column(column.id, column.name);
-//         setupCards(col, column.cards)
-//         board.addColumn(col);
-//     });
-// }
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
+        setupCards(col, column.cards)
+        board.addColumn(col);
+    });
+}
 
-// function setupCards(col, cards) {
-//     cards.forEach(function(card) {
-//         var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-//         col.addCard(card);
-//     });
-// }
+function setupCards(col, cards) {
+    cards.forEach(function(card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.addCard(card);
+    });
+}
 
-// $.ajaxSetup({
-//     headers: myHeaders
-// });
+$.ajaxSetup({
+    headers: myHeaders
+});
 
-// $.ajax({
-//     url: baseUrl + "/board",
-//     method: "GET",
-//     success: function(response) {
-//         setupColumns(response.columns);
-//     }
-// });
+$.ajax({
+    url: baseUrl + "/board",
+    method: "GET",
+    success: function(response) {
+        setupColumns(response.columns);
+    }
+});
